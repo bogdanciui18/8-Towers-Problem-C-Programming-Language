926a9b17a1a14ab3d5bac2827522810fa5201810
Bogdan Ciui <138584489+bogdanciui18@users.noreply.github.com>
2024-01-16 19:11:31 +0200

    Update script.c

--- script.c
+++ script.c
@@ -24,27 +24,21 @@ void printBoard(int board[N][N], int solutionNumber) {
 
     printf("\n");
 }
-    function isValid(row, col) {
-        for (let i = 0; i < n; i++) {
-            if (board[row][i] === 1 || board[i][col] === 1) {
-                return false;
-            }
-        }
+  bool isValid(int board[N][N], int row, int col) {
+    for (int i = 0; i < col; i++)
+        if (board[row][i])
+            return false;
 
-        for (let i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] === 1) {
-                return false;
-            }
-        }
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j])
+            return false;
 
-        for (let i = row, j = col; i < n && j >= 0; i++, j--) {
-            if (board[i][j] === 1) {
-                return false;
-            }
-        }
+    for (int i = row, j = col; i < N && j >= 0; i++, j--)
+        if (board[i][j])
+            return false;
 
-        return true;
-    }
+    return true;
+}
 
     function solveTowersProblem(col) {
         if (col >= n) {
